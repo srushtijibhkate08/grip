b6c9f18e6a1c44db03f279669b7688e9b31d7087
srushtijibhkate08 <139737308+srushtijibhkate08@users.noreply.github.com>
2023-07-31 18:10:52 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,26 +1,23 @@
-#!/usr/bin/env bash
+@echo off
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
+echo The following Maven command installs your Maven-built Java application
+echo into the local Maven repository, which will ultimately be stored in
+echo Jenkins's local Maven repository (and the "maven-repository" Docker data
+echo volume).
+
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-set +x
+echo The following complex command extracts the value of the ^<name/^> element
+echo within ^<project/^> of your Java/Maven project's "pom.xml" file.
 
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
+for /f "tokens=*" %%i in ('mvn help:evaluate -Dexpression=project.name ^| findstr /r /v "\["') do set "NAME=%%i"
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+echo The following complex command behaves similarly to the previous one but
+echo extracts the value of the ^<version/^> element within ^<project/^> instead.
+
+for /f "tokens=*" %%i in ('mvn help:evaluate -Dexpression=project.version ^| findstr /r /v "\["') do set "VERSION=%%i"
+
+echo The following command runs and outputs the execution of your Java
+echo application (which Jenkins built using Maven) to the Jenkins UI.
+
+java -jar target\%NAME%-%VERSION%.jar
